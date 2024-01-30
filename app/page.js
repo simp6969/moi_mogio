f79dd7e89efdbcc5cdb68fbaa1be3c4ada00e623
bold ariunbold <ariunboldbold200@gmail.com>
2024-01-31 05:24:12 +0800

    core functions

--- app/page.js
+++ app/page.js
@@ -3,11 +3,9 @@
 import { useEffect, useState } from "react";
 export default function Mogoi() {
   const [isSpaceClicked, setIsSpaceClicked] = useState(false);
-  const [currentKey, setCurrentKey] = useState();
-  const [currentpos, setCurrentPos] = useState();
-
-  function handleSnake() {}
-
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [playerDied, setPlayerDied] = useState(false);
   useEffect(() => {
     if (document) {
       document.onkeydown = (e) => {
@@ -15,27 +13,40 @@ export default function Mogoi() {
           setIsSpaceClicked(true);
         }
         if (e.key === "ArrowRight") {
-          setCurrentKey("translateX(20px)");
+          setX(x + 1);
         }
         if (e.key === "ArrowLeft") {
-          setCurrentKey("translateX(-20px)");
+          setX(x - 1);
         }
         if (e.key === "ArrowUp") {
-          setCurrentKey("translateY(-20px)");
+          setY(y - 1);
         }
         if (e.key === "ArrowDown") {
-          setCurrentKey("translateY(20px)");
+          setY(y + 1);
         }
       };
     }
-  }, []);
+    if (y > 24) {
+      setY(-24);
+    }
+    if (y < -24) {
+      setY(24);
+    }
+    if (x > 24) {
+      setX(-24);
+    }
+    if (x < -24) {
+      setX(24);
+    }
+  }, [x, y]);
+  console.log(x, y);
   return (
     <div className="flex justify-center items-center h-[100vh] w-[100vw]">
       {isSpaceClicked ? null : <div>press space to start</div>}
       {isSpaceClicked ? (
         <div className="w-[500px] h-[500px] bg-[#2c2c2c] rounded-[20px] flex justify-center items-center">
           <div
-            style={{ transform: currentKey }}
+            style={{ position: "relative", top: y * 10, left: x * 10 }}
             className="h-[20px] w-[20px] bg-[red]"
           ></div>
         </div>
